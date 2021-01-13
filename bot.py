c65d9fa8df6219a260551939d8fa5741fde341e9
Pranjal Vachaspati <pr@nj.al>
2021-01-13 01:54:11 -0500

    Add pin_messages bot extension

--- bot.py
+++ bot.py
@@ -17,15 +17,12 @@ import puzzboss_interface
 from common import *
 from discord.ext import commands
 from discord.ext.commands import guild_only
-from pytz import timezone
 from config import config
 from discord_info import is_puzzle_channel
 
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
-
-startup_extensions = ["solving_tools"]
 
 default_help = commands.DefaultHelpCommand(
     no_category="Commands to try",
@@ -42,38 +39,6 @@ bot = commands.Bot(
 async def on_ready():
     logging.info("Connected as {0.user} and ready!".format(bot))
 
-
-
-@bot.listen("on_raw_reaction_add")
-async def handle_reacts(payload):
-    if payload.user_id == bot.user.id:
-        return
-    if not payload.guild_id:
-        return
-    emoji = str(payload.emoji)
-    if emoji not in "🧩📌🧹":
-        return
-    guild = bot.get_guild(payload.guild_id)
-    if not guild:
-        return
-    channel = guild.get_channel(payload.channel_id)
-    if not channel:
-        return
-    message = await channel.fetch_message(payload.message_id)
-    if not message:
-        return
-    if emoji == "📌":
-        if not message.pinned:
-            await message.pin()
-            await message.clear_reaction("🧹")
-        return
-    if emoji == "🧹":
-        if message.pinned:
-            await message.unpin()
-            await message.clear_reaction("📌")
-            await message.clear_reaction("🧹")
-        return
-    
 if __name__ == "__main__":
     # Define logging levels
     loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
---
+++ bot_extensions/pin_messages.py
@@ -0,0 +1,42 @@
+""" Lets users pin and unpin messages with emoji reacts """
+import discord
+from discord.ext import commands
+
+class PinMessages(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener("on_raw_reaction_add")
+    async def handle_reacts(self, payload):
+        if payload.user_id == self.bot.user.id:
+            return
+        if not payload.guild_id:
+            return
+        emoji = str(payload.emoji)
+        if emoji not in "📌🧹":
+            return
+        guild = self.bot.get_guild(payload.guild_id)
+        if not guild:
+            return
+        channel = guild.get_channel(payload.channel_id)
+        if not channel:
+            return
+        message = await channel.fetch_message(payload.message_id)
+        if not message:
+            return
+        if emoji == "📌":
+            if not message.pinned:
+                await message.pin()
+                await message.clear_reaction("🧹")
+            return
+        if emoji == "🧹":
+            if message.pinned:
+                await message.unpin()
+                await message.clear_reaction("📌")
+                await message.clear_reaction("🧹")
+            return
+    
+def setup(bot):
+    cog = PinMessages(bot)
+    bot.add_cog(cog)
+        
