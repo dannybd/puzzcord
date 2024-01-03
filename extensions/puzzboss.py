a84043340cc0464bb3c71f7884d7fad3c8fe2658
dannybd <dannybd@mit.edu>
2024-01-03 22:40:51 +0000

    eventually I will crack this name thing discord.py has three versions

--- extensions/puzzboss.py
+++ extensions/puzzboss.py
@@ -14,9 +14,11 @@ def print_user(user: discord.Member, with_display_name=False):
     name = user.name
     if int(user.discriminator or 0):
         name += f"#{user.discriminator}"
-    local_name = str(user)
-    if with_display_name and local_name != name:
-        name += f" ({local_name})"
+    if with_display_name:
+        if user.display_name != name:
+            name += f" ({user.display_name})"
+        elif user.global_name != name:
+            name += f" ({user.global_name})"
     return name
 
 
